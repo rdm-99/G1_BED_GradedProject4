852694a67a5a803db34398b8d3f82268ad60808f
Rohan Deb Mondal <95281095+rdm-99@users.noreply.github.com>
2024-03-31 20:22:07 +0530

    Add files via upload

--- employeemanagement/src/main/java/com/greatlearning/repository/EmployeeRepository.java
+++ employeemanagement/src/main/java/com/greatlearning/repository/EmployeeRepository.java
@@ -1,1 +1,9 @@
+package com.greatlearning.employeemanagement.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.greatlearning.employeemanagement.model.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
